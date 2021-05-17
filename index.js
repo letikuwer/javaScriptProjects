ccaffae2790e6b2d62520c97451f094ecca653f2
Letícia K <leticia.skuwer@gmail.com>
2021-05-17 17:08:04 -0300

    Incremento de 1 cliente

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ class Cliente{
     cpf;
     agencia;
     saldo;
+    rg;
 }
 const cliente1 = new Cliente ();
+const cliente2= new Cliente ();
 
 
 cliente1.nome ="Letícia";
 cliente1.cpf = 03831091090;
 cliente1.agencia = 1001;
 cliente1.saldo = 0;
+cliente1.rg = 4545454545
+
+cliente2.nome= "Alice";
+cliente2.cpf= 25845456464;
+cliente2.agencia= 1001;
+cliente2.saldo=0;
 
 console.log(cliente1);
+console.log(cliente2);
